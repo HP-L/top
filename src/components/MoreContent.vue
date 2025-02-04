7f352e5067f0c7e704ad9c7b564f9042d58cdfe8
HP-L <1801700@qq.com>
2025-02-04 18:00:32 +0800

    更新链接

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="more-content">春意盎然，绿叶是生命的旋律...</div>
-  <div class="more-content">（Ctrl+F5刷新）</div>
+  <div class="more-content">已过立春，壁纸不一致需Ctrl+F5刷新</div>
 </template>
 
 <style lang="scss" scoped>
